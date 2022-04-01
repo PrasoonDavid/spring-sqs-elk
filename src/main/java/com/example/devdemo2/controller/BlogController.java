fef961d5ce9619ce035050e8670807b700c96569
Prasoon David <prasoon.david@byjus.com>
2022-04-01 17:13:25 +0530

    Fuzzy search added

--- src/main/java/com/example/devdemo2/controller/BlogController.java
+++ src/main/java/com/example/devdemo2/controller/BlogController.java
@@ -2,25 +2,43 @@ package com.example.devdemo2.controller;
 
 import com.amazonaws.services.sqs.model.SendMessageResult;
 import com.example.devdemo2.model.Blog;
+import com.example.devdemo2.service.BlogService;
 import com.example.devdemo2.service.sqs.QueueService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/blog")
 public class BlogController {
     private final QueueService queueService;
-
+    private final BlogService blogService;
     @Autowired
-    public BlogController(QueueService queueService) {
+    public BlogController(QueueService queueService, BlogService blogService) {
         this.queueService = queueService;
+        this.blogService = blogService;
     }
 
 
     @PostMapping(value = "/send", produces = MediaType.APPLICATION_JSON_VALUE)
     public SendMessageResult sendmessage(@RequestBody Blog blogObject) throws JsonProcessingException{
         return queueService.sendSqsMessage("queue-2",blogObject);
+    }
+    @GetMapping(value= "/elk/{id}")
+    public Blog findById(@PathVariable final String id){
+        return blogService.findById(id);
+    }
+
+    @GetMapping(value= "/elk/")
+    public Iterable<Blog> findAll(){
+        return blogService.findAll();
+    }
+
+    @GetMapping(value="/elk/find/{post}")
+    public List<Blog> findByPost(@PathVariable final String post){
+        return blogService.findByPost(post);
     }
 }
