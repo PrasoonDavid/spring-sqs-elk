b26622b00af709cd1b3968ecece30f6ffee92876
Prasoon David <prasoon.david@byjus.com>
2022-04-01 17:05:52 +0530

    Index added

--- src/main/java/com/example/devdemo2/model/Blog.java
+++ src/main/java/com/example/devdemo2/model/Blog.java
@@ -6,10 +6,13 @@ import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import static com.example.devdemo2.helper.Indices.BLOG_INDEX;
-
+@Document(indexName = BLOG_INDEX)
 public class Blog {
+    @Id
+    @Field(type = FieldType.Keyword)
     private String id;
 
+    @Field(type = FieldType.Text)
     private String post;
 
     public String getId() {
